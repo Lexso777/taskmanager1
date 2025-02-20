64674e432799621b22053e0f75b715ba1ee4d172
Lexso <siroporshnev.alexei@icloud.com>
2025-02-21 01:43:47 +0300

    Обновления во фронте

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import style from './LoginPage.module.css'
 import LoginForm from './forms/LoginForm';
 import RegisterForm from './forms/RegisterForm';
@@ -8,31 +8,9 @@ import RegisterForm from './forms/RegisterForm';
 const LoginPage = () => {
 
     const [activeTab, setActiveTab] = useState('login'); 
-    const [message, setMessage] = useState('');
-    const [error, setError] = useState('');
-
-    useEffect(() => {
-        fetch('http://localhost:8080/')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.text();
-            })
-            .then(data => {
-                setMessage(data);
-                setError(''); // Сбрасываем ошибку, если запрос успешен
-            })
-            .catch(error => {
-                console.error('Ошибка:', error);
-                setError('Не удалось загрузить данные с сервера.');
-            });
-    }, []);
 
     return (
         <div className={style.loginPage__container}>
-            <div>
-            </div>
             <div className={style.login__module__window}>
                 <div className={style.module__window__buttons}>
                     <button 
--- frontend/src/components/LoginPage/forms/LoginForm.jsx
+++ frontend/src/components/LoginPage/forms/LoginForm.jsx
@@ -1,49 +1,54 @@
-import React, { useRef, useState } from 'react';
-import style from './Form.module.css'
+import React, { useState, useRef } from 'react';
+import style from './Form.module.css';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchLogin } from '../../../Redux/slices/LoginSlice';
 
 const LoginForm = () => {
 
-
-    const [passwordVisible, setpasswordVisible] = useState(false);
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const navigate = useNavigate();
+    const {error} = useSelector((state) => state.LoginSlice)
+    const dispatch = useDispatch();
     const passwordInputRef = useRef(null);
 
 
-    const [password, setPassword] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = {
+            email: formData.get("email"),
+            password: formData.get("password"),
+            remember: formData.get("agree") ? true : false
+        };
+
+        dispatch(fetchLogin(data)).then((result) => {
+            if(result.meta.requestStatus === 'fulfilled'){
+                navigate('/main');
+            }else{
+                console.log("Ошибка авторизации");
+            }
+        });
+    }   
 
 
     const togglePasswordVisibility = () => {
-        setpasswordVisible(!passwordVisible);
-
-        if (passwordInputRef.current) {
-            passwordInputRef.current.focus();
-        }
-    }
+        setPasswordVisible(!passwordVisible);
+        passwordInputRef.current.focus();
+    };
 
     return (
         <>
             <h2 className={style.h2}>Login</h2>
-            {/* <form method="POST" action="http://localhost:8080/create" className={style.test}>
-                <div>
-                    <label>Model</label>
-                    <input type="text" name="model" />
-                </div>
-<<<<<<< HEAD
-                <label className={style.label}>
-                    <input type="checkbox" name="agree"></input>
-                    <span>Remember me</span>
-                </label>
-                <button
-                    className={style.button}
-                // onClick={() => counted()}
-                >
-                    Login
-                </button> */}
-            <form method='POST' action="http://localhost:8080/create" className={style.form}>
+            <form method="POST" action="http://localhost:8080/login" onSubmit={handleSubmit} className={style.form}>
                 <input
                     className={style.input}
                     type="email"
+                    name="email"
                     autoComplete="username"
-                    placeholder='Логин'
+                    placeholder="Логин"
+                    required
                 />
                 <div className={style.password__container}>
                     <svg
@@ -65,22 +70,22 @@ const LoginForm = () => {
                     <input
                         ref={passwordInputRef}
                         className={style.input}
-                        type={passwordVisible ? 'text' : 'password'}
+                        type={passwordVisible ? "text" : "password"}
                         autoComplete="current-password"
-                        placeholder='Пароль'
-                        onChange={(e) => setPassword(e.target.value)}
+                        name="password"
+                        placeholder="Пароль"
+                        required
                     />
                 </div>
+                <div className={style.error}>{error}</div>
                 <label className={style.label}>
-                    <input type="checkbox" name="agree"></input>
+                    <input type="checkbox" name="agree" />
                     <span>Remember me</span>
                 </label>
-                <button
-                    className={style.button}
-                // onClick={() => counted()}
-                >
+                <button className={style.button} type="submit">
                     Login
                 </button>
+                
             </form>
         </>
     );
